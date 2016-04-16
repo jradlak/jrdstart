6d9364ed149524626501ae64c04efdab0add171d
Jakub Radlak <jradlak@gmail.com>
2016-04-16 21:48:57 +0200

    liquibase enabled

--- src/main/java/com/jrd/jrdstart/Application.java
+++ src/main/java/com/jrd/jrdstart/Application.java
@@ -2,8 +2,11 @@ package com.jrd.jrdstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ LiquibaseProperties.class })
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/com/jrd/jrdstart/config/DatabaseConfiguration.java
+++ src/main/java/com/jrd/jrdstart/config/DatabaseConfiguration.java
@@ -1,7 +1,38 @@
 package com.jrd.jrdstart.config;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
 /**
  * Created by Kuba on 2016-04-16.
  */
+@Configuration
+@EnableJpaRepositories("com.jrd.jrdstart.repository")
+@EnableTransactionManagement
 public class DatabaseConfiguration {
+    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource, LiquibaseProperties liquibaseProperties) {
+
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setShouldRun(liquibaseProperties.isEnabled());
+
+        log.debug("Configuring Liquibase");
+
+        return liquibase;
+    }
 }
